be351a98305e4794ba01327e7562dc884f3aff62
lithid <long.jeremie@gmail.com>
2011-07-05 07:57:23 -0700

    Use buzzer for alarm by default as well.

--- products/common_full.mk
+++ products/common_full.mk
@@ -8,4 +8,4 @@ include frameworks/base/data/sounds/AudioPackage2.mk
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Thunderfoot.ogg \
     ro.config.notification_sound=Beat_Box_Android.ogg \
-    ro.config.alarm_alert=Alarm_Beep_03.ogg
+    ro.config.alarm_alert=Alarm_Buzzer.ogg

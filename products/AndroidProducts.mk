67ec1ff20ac8e856f9e8fbe2aa5297c5af4141e7
lithid <long.jeremie@gmail.com>
2011-07-07 14:12:17 -0400

    remove inc and dream

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,3 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/salvage_supersonic.mk \
-    $(LOCAL_DIR)/salvage_incredible.mk \
     $(LOCAL_DIR)/salvage_generic.mk \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,3 @@
 add_lunch_combo salvage_supersonic-userdebug
-add_lunch_combo salvage_incredible-userdebug
-add_lunch_combo salvage_dream_sapphire-userdebug
 
 PATH=$PATH:$PWD/vendor/salvage/tools ; export PATH
